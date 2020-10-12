f467c88b482687cf734a21b82cb60d88e39fca64
tlr24 <tlr24@njit.edu>
2020-10-12 14:50:16 -0400

    Added username and profile feature

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -16,4 +16,32 @@ function has_role($role){
     }
     return false;
 }
+function get_username() {
+    if (is_logged_in() && isset($_SESSION["user"]["username"])) {
+        return $_SESSION["user"]["username"];
+    }
+    return "";
+}
+
+function get_email() {
+    if (is_logged_in() && isset($_SESSION["user"]["email"])) {
+        return $_SESSION["user"]["email"];
+    }
+    return "";
+}
+
+function get_user_id() {
+    if (is_logged_in() && isset($_SESSION["user"]["id"])) {
+        return $_SESSION["user"]["id"];
+    }
+    return -1;
+}
+
+function safer_echo($var) {
+    if (!isset($var)) {
+        echo "";
+        return;
+    }
+    echo htmlspecialchars($var, ENT_QUOTES, "UTF-8");
+}
 ?>
