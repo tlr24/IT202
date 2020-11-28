f8f8a2389730cc5bb6f9bd0ff5daa1f3b7cbfb9e
tlr24 <tlr24@njit.edu>
2020-11-28 16:33:40 -0500

    User can clear cart & fixed the split form

--- project/view_cart.php
+++ project/view_cart.php
@@ -10,6 +10,13 @@ if (!is_logged_in()) {
 $db = getDB();
 $total = 0;
 
+if(isset($_POST["delete_all"])){
+    $stmt = $db->prepare("DELETE FROM Cart where user_id = :uid");
+    $r = $stmt->execute([":uid"=>get_user_id()]);
+    if($r){
+        flash("Deleted all items from cart", "success");
+    }
+}
 if(isset($_POST["delete"])){
     $stmt = $db->prepare("DELETE FROM Cart where id = :id AND user_id = :uid");
     $r = $stmt->execute([":id"=>$_POST["cartId"], ":uid"=>get_user_id()]);
@@ -46,9 +53,12 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h1>Cart</h1>
     <div class="list-group">
 <?php if($results && count($results) > 0):?>
+<div>
+    <input type="submit" class="btn btn-danger" name="delete_all" value="Delete All Items" form="form"/>
+</div>
     <?php foreach($results as $r):?>
         <div class="list-group-item">
-            <form method="POST">
+            <form method="POST" id="form">
                 <div class="row">
                     <div class="col">
                         <b><?php echo $r["name"];?></b>
@@ -58,7 +68,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <button type="button" onClick="document.location.href='view_product.php?id=<?php safer_echo($r['product_id']); ?>'">View</button>
                     </div>
                     <div class="col">
-                        <b>Quantity</b>
+                        <b>Quantity:</b>
                         <input type="number" min="0" name="quantity" step="1" value="<?php echo $r["quantity"];?>"/>
                         <input type="hidden" name="cartId" value="<?php echo $r["id"];?>"/>
                     </div>
@@ -68,12 +78,10 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <?php $total += floatval($r["sub"]);?>
                     </div>
                     <div class="col">
-                        <!-- form split was on purpose-->
                         <input type="submit" class="btn btn-success" name="update" value="Update"/>
-            </form>
-            <form method="POST">
-                <input type="hidden" name="cartId" value="<?php echo $r["id"];?>"/>
-                <input type="submit" class="btn btn-danger" name="delete" value="Delete"/>
+                        <input type="hidden" name="cartId" value="<?php echo $r["id"];?>" form="form"/>
+                        <input type="submit" class="btn btn-danger" name="delete" value="Delete" form="form"/>
+
             </form>
         </div>
         </div>
