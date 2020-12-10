231c988cda1dfe6e8ebd6e25f18f6608f38df278
tlr24 <tlr24@njit.edu>
2020-12-09 21:35:34 -0500

    added order number to each item

--- project/purchase_history.php
+++ project/purchase_history.php
@@ -9,8 +9,8 @@ if (!is_logged_in()) {
 }
 
 $db = getDB();
-$user_query = "SELECT o.user_id, o.created, oi.product_id, oi.quantity, oi.unit_price, (oi.unit_price * oi.quantity) as sub FROM Orders as o JOIN OrderItems as oi on oi.order_id = o.id where o.user_id = :id LIMIT 10";
-$admin_query = "SELECT o.user_id, o.created, oi.product_id, oi.quantity, oi.unit_price, (oi.unit_price * oi.quantity) as sub FROM Orders as o JOIN OrderItems as oi on oi.order_id = o.id LIMIT 10";
+$user_query = "SELECT o.id, o.user_id, o.created, oi.product_id, oi.quantity, oi.unit_price, (oi.unit_price * oi.quantity) as sub FROM Orders as o JOIN OrderItems as oi on oi.order_id = o.id where o.user_id = :id LIMIT 10";
+$admin_query = "SELECT o.id, o.user_id, o.created, oi.product_id, oi.quantity, oi.unit_price, (oi.unit_price * oi.quantity) as sub FROM Orders as o JOIN OrderItems as oi on oi.order_id = o.id LIMIT 10";
 $stmt = $db->prepare(has_role("Admin")?$admin_query:$user_query);
 $stmt->execute([":id"=>get_user_id()]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,6 +26,7 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <h3><b><?php safer_echo(getProductName($r["product_id"])); ?></b></h3>
                 <?php if(has_role("Admin")):?><div><b>User: </b><?php safer_echo(get_username_from_id($r["user_id"])); ?></div><?php endif; ?>
                 <div><b>Date: </b><?php safer_echo($r["created"]); ?></div>
+                <div><b>Order: </b>#<?php safer_echo($r["id"]); ?></div>
                 <div><b>Price: </b>$<?php safer_echo($r["unit_price"]); ?></div>
                 <div><b>Quantity: </b><?php safer_echo($r["quantity"]); ?></div>
                 <div><b>Subtotal: </b>$<?php safer_echo($r["sub"]); ?></div>
